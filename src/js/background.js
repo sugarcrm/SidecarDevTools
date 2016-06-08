6e3f7d3a7106b7334b29084c7c8bfefa5773bead
Filipe Guerra <alias.mac@gmail.com>
2016-06-08 03:09:57 -0700

    Simplify enable/disable with browser action

--- src/js/background.js
+++ src/js/background.js
@@ -18,3 +18,45 @@ chrome.extension.onConnect.addListener(function(port) {
     });
 });
 
+/**
+ * Holds the state of debug mode. True means we should debug the page.
+ */
+let debug = false;
+let hasAPIs = chrome.webRequest;
+
+/**
+ * Responds to clicks on the extension's icon. Toggles debug mode.
+ */
+chrome.browserAction.onClicked.addListener((tab) => {
+
+    debug = !debug;
+    chrome.browserAction.setTitle({
+        title: debug ? 'Sidecar Debug: ON' : 'Sidecar Debug: OFF'
+    });
+    // chrome.browserAction.setIcon({
+    //     path: debug ? 'icon-on.png' : 'icon-off.png'
+    // });
+    chrome.browserAction.setBadgeText({
+        text: debug ? 'ON' : ''
+    });
+
+    chrome.tabs.update(tab.id, {url: tab.url, selected: tab.selected}, null);
+
+    // webRequest handlers changed (solves caching issues)
+    hasAPIs && chrome.webRequest.handlerBehaviorChanged();
+});
+
+hasAPIs && chrome.webRequest.onCompleted.addListener((details) => {
+
+    // if we are in debug mode or the script is actually a sourcemap, skip
+    // the attachment (was probably added already from `sidecar.min.js` or
+    // `sidecar.js`)
+    if (!debug || details.url.indexOf('sidecar.min.js.map') > 0) {
+        return;
+    }
+
+    chrome.tabs.executeScript(details.tabId, {
+        file: 'js/inject/attach.js',
+    });
+
+}, {urls: ['*://*/*/sidecar.js*', '*://*/*/sidecar.min.js*']});
--- src/js/inject/content_script.js
+++ src/js/inject/content_script.js
@@ -6,14 +6,6 @@
     var url = chrome.extension.getURL('js/inject/');
     document.documentElement.setAttribute('sdt-path', url);
 
-    // inject attach.js which can access existing JavaScript
-    var s = document.createElement('script');
-    s.src = chrome.extension.getURL("js/inject/attach.js");
-    s.onload = function() {
-        this.parentNode.removeChild(this);
-    };
-    (document.head||document.documentElement).appendChild(s);
-
     //Handler request from background page
     chrome.runtime.onMessage.addListener(function(message, sender) {
 
