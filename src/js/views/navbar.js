a535f14174000a007d9c39706e512a01f1067d55
Yvan Gadeau <ygadeau@sugarcrm.com>
2016-06-10 08:19:47 -0700

    Add alert message in templates for modules that needs the to have the debug mode activated.

--- src/js/views/navbar.js
+++ src/js/views/navbar.js
@@ -17,44 +17,22 @@
     },
 
     events: {
-      'click a': 'selectItem',
-      'keydown': 'keydown'
+      'click a': 'selectItem'
     },
 
     selectItem: function(evt) {
-      evt.preventDefault();
-      var anchor = $(evt.currentTarget);
-      anchor.addClass('selected').siblings().removeClass('selected');
-      this.trigger('select', anchor.attr('href'));
-    },
-
-    keydown: function(e) {
-      switch(e.keyCode) {
-        case 74: // j
-        case 40: // down
-          this.nextItem();
-          break;
-        case 75: // k
-        case 38: // up
-          this.previousItem();
-          break;
+      var callback = function(result, isException) {
+        if (isException) {
+        } else {
+            BDT.debugMode = result;
+            evt.preventDefault();
+            var anchor = $(evt.currentTarget);
+            anchor.addClass('selected').siblings().removeClass('selected');
+            this.trigger('select', anchor.attr('href'));
+        }
       }
+
+      BDT.page.eval('isDebugMode', [], _.bind(callback, this));
     },
-
-    nextItem: function() {
-      var selected = this.$('.selected');
-      if (selected.next().size() == 0) return;
-      var next = selected.removeClass('selected').next().addClass('selected');
-      this.trigger('select', $('a', next).attr('href'));
-    },
-
-    previousItem: function() {
-      var selected = this.$('.selected');
-      if (selected.prev().size() == 0) return;
-      var prev = selected.removeClass('selected').prev().addClass('selected');
-      this.trigger('select', $('a', prev).attr('href'));
-    }
-
   });
-
 })();
